72e9861796ff0ba3a469bd725f374667a2679d65
socnorb55 <46750451+socnorb55@users.noreply.github.com>
2022-11-11 13:29:23 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+  
+  backend "s3" {
+       bucket = "terraform-state-bucket-aws-cloud-sandbox"
+       key    = "arn:aws:kms:us-east-2:530869569617:key/c53c951f-5d82-45f0-aa68-510624dc6ec9"
+       region = "east-us-2"
+   }
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-2"
+}
