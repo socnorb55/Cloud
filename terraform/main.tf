617a7621405e0f6c08877a7358f6f09c348a7179
socnorb55 <46750451+socnorb55@users.noreply.github.com>
2022-11-16 18:01:19 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,18 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "3.74.0"
     }
   }
-
-  required_version = ">=0.14.9"
-  
   backend "s3" {
        bucket = "terraform-state-bucket-aws-cloud-sandbox"
        key    = "arn:aws:kms:us-east-2:530869569617:key/c53c951f-5d82-45f0-aa68-510624dc6ec9"
        region = "us-east-2"
+       encrypt = true
    }
 
 }
 
 provider "aws" {
-  version = "~>3.0"
   region  = "us-eat-2"
 }
