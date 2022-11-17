da287732d1bfaeea8cc9f97253757d804c702ec7
socnorb55 <socnorb55@gmail.com>
2022-11-17 18:32:19 -0500

    removed source hash

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,7 +6,6 @@ resource "aws_lambda_function" "test_lambda" {
     function_name ="test_lambda-${var.env}"
     role ="arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role"
     handler ="src.hello_world.lambda_handler"
-    source_code_hash = filebase64sha256("${path.module}/../src.zip")
     layers =[aws_lambda_layer_version.python_packages_layer.arn]
 }
 
