252009450fd82d461a44a10aa02c162720707188
socnorb55 <socnorb55@gmail.com>
2022-11-17 18:10:24 -0500

    changing lambda layer file path

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -12,6 +12,6 @@ resource "aws_lambda_function" "test_lambda" {
 
 resource "aws_lambda_layer_version" "python_packages_layer" {
     layer_name ="lambda_layer_${var.env}"
-    filename ="${path.root}/../lambda_layers.zip"
-    source_code_hash = filebase64sha256("${path.root}/../lambda_layers.zip")
+    filename ="${path.root}/lambda_layers.zip"
+    source_code_hash = filebase64sha256("${path.root}/lambda_layers.zip")
 }
