14e328e5c5641bd30e2ac690ad6da1ed893d7f0a
socnorb55 <socnorb55@gmail.com>
2022-11-17 19:28:26 -0500

    zip path

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,11 +6,5 @@ resource "aws_lambda_function" "test_lambda" {
     function_name ="test_lambda-${var.env}"
     role ="arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role"
     handler ="src.hello_world.lambda_handler"
-    layers =[aws_lambda_layer_version.python_packages_layer.arn]
     runtime = "python3.9"
-}
-
-resource "aws_lambda_layer_version" "python_packages_layer" {
-    layer_name ="lambda_layer_${var.env}"
-    filename ="${path.root}/lambda_layers.zip"
 }
