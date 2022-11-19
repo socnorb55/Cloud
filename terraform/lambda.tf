b971065e17b06724843d2c7f972d92fd8daa5110
socnorb55 <socnorb55@gmail.com>
2022-11-19 11:16:25 -0500

    adding data archive

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,6 +2,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current_region" {}
 
 resource "aws_lambda_function" "test_lambda" {
+    filename = data.archive_file.src.output_path
     function_name ="test_lambda-${var.env}"
     role ="arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role"
     handler ="src.hello_world.lambda_handler"
